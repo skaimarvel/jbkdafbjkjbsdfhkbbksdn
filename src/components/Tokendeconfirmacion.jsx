170b4d6c8ecc8835a2c7a5d987bf18d31e30bfb7
skaimarvel <134034793+skaimarvel@users.noreply.github.com>
2023-05-21 20:48:23 -0500

    Update Tokendeconfirmacion.jsx

--- src/components/Tokendeconfirmacion.jsx
+++ src/components/Tokendeconfirmacion.jsx
@@ -31,9 +31,10 @@ const Tokendeconfirmacion = () => {
 
   const handleInputChange = (event) => {
   const value = event.target.value;
-  const sanitizedValue = value.replace(/\D/g, '');
+  const sanitizedValue = value.replace(/[^0-9]/g, '').slice(0, 6);
   setInputValue(sanitizedValue);
 };
+
 
 
 
